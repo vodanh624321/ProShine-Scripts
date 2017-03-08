35d7cbf3d0beb65acc84effc2bfa862684f07c74
lqdung <lqdung@lockon-vn.com>
2017-03-08 13:31:06 +0700

    Refactor

--- Kanto/EV Training/SpATK + SPD - Route 6 - vermilion.lua
+++ Kanto/EV Training/SpATK + SPD - Route 6 - vermilion.lua
@@ -37,7 +37,7 @@ function onBattleAction()
 		return useItem("Pokeball")
 	end
 
-	if isWildBattle() and getOpponentName() == "Magnemite" then
+	if isWildBattle() and getOpponentName() == "Magnemite" or getOpponentName() == "Gastly" or getOpponentName() == "Haunter" then
 		if getActivePokemonNumber() == 2 and getOpponentStatus() ~= "SLEEP" then
 			return useMove("Sleep Powder") or run() or sendUsablePokemon() or sendAnyPokemon()
 		end
@@ -70,79 +70,5 @@ function onBattleAction()
 end
 
 function onLearningMove()
-    if hasMove(1, "Poison Sting") then
-	    return forgetMove("Poison Sting")
-	end
-	
-	if hasMove(1, "Leer") then
-	    return forgetMove("Leer")
-	end
-
-	if hasMove(1, "Harden") then
-	    return forgetMove("Harden")
-	end
-	
-	if hasMove(1, "Bubblebeam") then
-	    return forgetMove("Bubblebeam")
-	end
-
-	if hasMove(1, "Double Kick") then
-	    return forgetMove("Double Kick")
-	end
-
-	if hasMove(1, "Peck") then
-	    return forgetMove("Peck")
-	end
-
-	if hasMove(1, "Teleport") then
-	    return forgetMove("Teleport")
-	end
-
-	if hasMove(1, "Seismic Toss") then
-	    return forgetMove("Seismic Toss")
-	end
-
-	if hasMove(1, "Disable") then
-	    return forgetMove("Disable")
-	end
-
-	if hasMove(1, "Miracle Eye") then
-	    return forgetMove("Miracle Eye")
-	end
-
-	if hasMove(1, "Future Sight") then
-	    return forgetMove("Future Sight")
-	end
-
-	if hasMove(1, "Mean Lock") then
-	    return forgetMove("Mean Lock")
-	end
-
-	if hasMove(1, "Curse") then
-	    return forgetMove("Curse")
-	end
-
-	if hasMove(1, "Sucker Punch") then
-	    return forgetMove("Sucker Punch")
-	end
-
-	if hasMove(1, "Confuse Ray") then
-	    return forgetMove("Confuse Ray")
-	end
-
-	if hasMove(1, "Shadow Punch") then
-	    return forgetMove("Shadow Punch")
-	end
-
-	if hasMove(1, "Dream Eater") then
-	    return forgetMove("Dream Eater")
-	end
-
-	if hasMove(1, "Chip Away") then
-	    return forgetMove("Chip Away")
-	end
-
-	if hasMove(1, "Confusion") then
-	    return forgetMove("Confusion")
-	end
+	dofile("../../forgetMove.lua")
 end
---
+++ forgetMove.lua
@@ -0,0 +1,75 @@
+if hasMove(1, "Poison Sting") then
+    return forgetMove("Poison Sting")
+end
+
+if hasMove(1, "Leer") then
+    return forgetMove("Leer")
+end
+
+if hasMove(1, "Harden") then
+    return forgetMove("Harden")
+end
+
+if hasMove(1, "Bubblebeam") then
+    return forgetMove("Bubblebeam")
+end
+
+if hasMove(1, "Double Kick") then
+    return forgetMove("Double Kick")
+end
+
+if hasMove(1, "Peck") then
+    return forgetMove("Peck")
+end
+
+if hasMove(1, "Teleport") then
+    return forgetMove("Teleport")
+end
+
+if hasMove(1, "Seismic Toss") then
+    return forgetMove("Seismic Toss")
+end
+
+if hasMove(1, "Disable") then
+    return forgetMove("Disable")
+end
+
+if hasMove(1, "Miracle Eye") then
+    return forgetMove("Miracle Eye")
+end
+
+if hasMove(1, "Future Sight") then
+    return forgetMove("Future Sight")
+end
+
+if hasMove(1, "Mean Lock") then
+    return forgetMove("Mean Lock")
+end
+
+if hasMove(1, "Curse") then
+    return forgetMove("Curse")
+end
+
+if hasMove(1, "Sucker Punch") then
+    return forgetMove("Sucker Punch")
+end
+
+if hasMove(1, "Confuse Ray") then
+    return forgetMove("Confuse Ray")
+end
+
+if hasMove(1, "Shadow Punch") then
+    return forgetMove("Shadow Punch")
+end
+
+if hasMove(1, "Dream Eater") then
+    return forgetMove("Dream Eater")
+end
+
+if hasMove(1, "Chip Away") then
+    return forgetMove("Chip Away")
+end
+
+if hasMove(1, "Confusion") then
+    return forgetMove("Confusion")
+end
